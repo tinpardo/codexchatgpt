c718cd4039e92ee5ac7037416d4550f1d8252c2b
Andres Pardo <tinpardo@gmail.com>
2025-06-06 00:35:13 -0400

    Integrate Clerk with env config

---
+++ modules/clerk.js
@@ -0,0 +1,7 @@
+export const clerkOptions = {
+  publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  signInUrl: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+  signUpUrl: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+  afterSignInUrl: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
+  afterSignUpUrl: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL,
+};
